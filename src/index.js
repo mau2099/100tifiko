ff02b057919eb62b288759b65dc9793c28403ead
Mauricio Garcia <fidel.garcia@globant.com>
2020-04-11 22:30:08 -0500

    se implementan las rutas por medio de las utilerias creadas getHash y resolveRoutes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 console.log('Hello Mau2099 🐧');
+
 import router from './routes/index';
 window.addEventListener('load', router);
+
+window.addEventListener('hashchange', router);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
 
+import getHash from '../utils/getHash';
+import resolveRoute from '../utils/resolveRoutes';
+
 const routes = {
   '/': Home,
   '/:id': Character,
@@ -14,6 +17,12 @@ const router = async () => {
   const content = null || document.getElementById('content');
 
   header.innerHTML = await Header();
+
+  let hash = getHash();
+  let route = await resolveRoute(hash);
+  let render = routes[route] ? routes[route] : Error404;
+
+  content.innerHTML = await render();
 };
 
 export default router;
